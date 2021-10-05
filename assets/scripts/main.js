c03a184603ed09089a54e3f2a75d29897d57971b
Chris Platts <chris@virelai.co.uk>
2021-10-05 11:12:29 +0100

    Testing jump memnu build 1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,25 @@
 $(function() {
 
+/**
+ * Build jump menu
+ */
+var $article-h2 = $('#content').find('.article-body').find('h2');
+if ($article-h2) {
+    consiole.log($article-h2);
+}
+
+/**
+ * Handle jump menu clicks: Disable default action and scroll slowly to target
+ */
 $(".jump-menu a").on("click", function (e) {
     e.preventDefault();
     var target = $(this).attr("href");
     $('html,body').animate({scrollTop: $(target).offset().top},'slow');
 });
 
+/**
+ * Handle H2 'Back to top' click
+ */
 $("h2").on("click", function () {
     $('html,body').animate({scrollTop: 0},'slow');
 });
